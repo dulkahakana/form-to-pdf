bd5d2a92df53bc9488cf5933ffac21afc44d65c1
Anton Karpov <81901285+dulkahakana@users.noreply.github.com>
2021-11-30 23:27:48 +0300

    Добавил обработку формы. Подлкючил стили

--- index.js
+++ index.js
@@ -1,39 +1,59 @@
 console.log(new Date())
 // подключение переменной из глобальной видимости
 const { jsPDF } = window.jspdf
+const doc = new jsPDF();
 
+const briefForm = document.getElementById('brief-form')
 
-// const printBox = document.getElementById('printBox')
-// const printBtn = document.getElementById('printBtn')
+briefForm.addEventListener('submit', retrieveFormValue)
 
-let doc = new jsPDF();
-// console.log(doc)
+function retrieveFormValue(event) {
+    event.preventDefault()
 
+    const fields = document.querySelectorAll('input, textarea')
+    const data = {}
 
-// doc.text('Test text', 10, 10)
-// doc.save('a4.pdf')
+    fields.forEach(field => {
+        const { name, value, type, checked } = field
+        data[name] = isCkeckboxOrRadio(type) ? checked : value;
+    })
 
-const specialElementHandlers = {
-    '#printBox': function (elements, renderer) {
-        return true
-    }
+    const formatedData = `ыфафыа`
+    doc.text(formatedData, 10, 10)
+    doc.save('a4.pdf')
 }
 
-// printBtn.addEventListener('click', createPDF)
+function isCkeckboxOrRadio(type) {
+    return ['checkbox', 'radio'].includes(type)
+}
 
-
-function createPDF () {
-    doc.fromHTML(printBox), 15, 15, {
-        'width': 170,
-        'elementHandlers': specialElementHandlers
-    }
-    doc.save('sample.pdf')
+function сreatePDF (text) {
+    doc.text(text, 10, 10)
+    doc.save('a4.pdf')
 }
 
 
+// const printBox = document.getElementById('brief-content')
+// const saveBtn = document.getElementById('saveBtn')
+
+// const specialElementHandlers = {
+//     '#printBox': function (elements, renderer) {
+//         return true
+//     }
+// }
+
+// saveBtn.addEventListener('click', () => {
+//     сreatePDF(testDataToPDF)
+// })
 
 
-
+// function createPDF () {
+//     doc.fromHTML(printBox), 15, 15, {
+//         'width': 170,
+//         'elementHandlers': specialElementHandlers
+//     }
+//     doc.save('sample.pdf')
+// }
 
 // Default export is a4 paper, portrait, using millimeters for units
 // const doc = new jsPDF();
