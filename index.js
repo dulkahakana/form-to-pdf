b4c250f5a2a630294305335feca747d54402adb6
Anton Karpov <81901285+dulkahakana@users.noreply.github.com>
2021-12-01 19:04:21 +0300

    добавлена библиотека html2pdf

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 console.log(new Date())
-// подключение переменной из глобальной видимости
-const { jsPDF } = window.jspdf
-const doc = new jsPDF();
 
+// dom элемент для вывода в pdf
+const printBox = document.getElementById('brief-content')
+
+// подключение к форме для получения данных
 const briefForm = document.getElementById('brief-form')
+
+const fileName = 'Ecliptic_logo_brief'
 
 briefForm.addEventListener('submit', retrieveFormValue)
 
 function retrieveFormValue(event) {
     event.preventDefault()
 
-    const fields = document.querySelectorAll('input, textarea')
-    const data = {}
-
-    fields.forEach(field => {
-        const { name, value, type, checked } = field
-        data[name] = isCkeckboxOrRadio(type) ? checked : value;
-    })
-
-    const formatedData = `ыфафыа`
-    doc.text(formatedData, 10, 10)
-    doc.save('a4.pdf')
+    // библиотека через канвас делает скрин и сохраняет его в pdf
+    html2pdf().from(printBox).save(fileName)
 }
 
 function isCkeckboxOrRadio(type) {
@@ -31,35 +25,3 @@ function сreatePDF (text) {
     doc.text(text, 10, 10)
     doc.save('a4.pdf')
 }
-
-
-// const printBox = document.getElementById('brief-content')
-// const saveBtn = document.getElementById('saveBtn')
-
-// const specialElementHandlers = {
-//     '#printBox': function (elements, renderer) {
-//         return true
-//     }
-// }
-
-// saveBtn.addEventListener('click', () => {
-//     сreatePDF(testDataToPDF)
-// })
-
-
-// function createPDF () {
-//     doc.fromHTML(printBox), 15, 15, {
-//         'width': 170,
-//         'elementHandlers': specialElementHandlers
-//     }
-//     doc.save('sample.pdf')
-// }
-
-// Default export is a4 paper, portrait, using millimeters for units
-// const doc = new jsPDF();
-
-// doc.text("Hello world!", 10, 10);
-// doc.save("a4.pdf");
-
-
-
