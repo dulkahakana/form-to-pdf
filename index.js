2f7370599ed1fa6cab1d3cf61afb6c542d6a80b5
Anton Karpov <81901285+dulkahakana@users.noreply.github.com>
2021-12-03 00:17:06 +0300

    обновлены стили. управление для radio button. вкл статическое разрешене для canvas

--- index.js
+++ index.js
@@ -4,28 +4,32 @@ console.log(new Date())
 const printBox = document.getElementById('brief-content')
 // dom элемент для отображения сформированной страницы
 const content = document.getElementById('brief-content')
-// получение размера страницы
-let pageWidth = document.documentElement.scrollWidth
-let pageHeight = document.documentElement.scrollHeight
-
-
 // подключение к форме для получения данных
 const briefForm = document.getElementById('brief-form')
 
 
+// получение размера страницы
+let pageWidth = document.documentElement.scrollWidth
+let pageHeight = document.documentElement.scrollHeight
 
+// прослушивание собития sunbit на форме
 briefForm.addEventListener('submit', retrieveFormValue)
 
+onOffCheckedList('logo-options__item', 'logo-options__input', 'logo-options__item__on')
+
+
+// функция которая срабатывает на sumbit по форме
 function retrieveFormValue(event) {
     event.preventDefault()
-    console.log('click btn')
+    console.log('sumbit')
 
     const fields = document.querySelectorAll('input, textarea')
     const data = {}
 
     const optionsLogo = document.querySelectorAll('.logo-options__input')
+    console.log(optionsLogo)
 
-    function getTrueChecked(arrCheckeds) {
+    function getTrueChecked(optionsLogo) {
         const result = []
         for (option of optionsLogo) {
             if (option.checked) {
@@ -46,28 +50,29 @@ function retrieveFormValue(event) {
     content.innerHTML = ''
 
     const title = createElement('div', 'brief-title')
-    title.textContent = 'Бриф на разработку логотипа'
+    title.textContent = `Бриф на разработку логотипа ${data.companyName}`
     content.appendChild(title)
     
     // формирование dom с данными из формы для вывода в pdf    
     createBriefItem(content, '1. НАЗВАНИЕ КОМПАНИИ/ПРОДУКТА, КОТОРОЕ ДОЛЖНО БЫТЬ НЕПОСРЕДСТВЕННО ОТРАЖЕНО В ЛОГОТИПЕ:', data.companyName)
     createBriefItem(content, '2. ОСНОВНЫЕ НАПРАВЛЕНИЯ ДЕЯТЕЛЬНОСТИ КОМПАНИИ/ОПИСАНИЕ ПРОДУКТА:', data.aboutCompany)
     createBriefItem(content, '3. КРИТИЧЕН ЛИ ДЛЯ ВАС ТАКОЙ ПАРАМЕТР КАК РЕГИСТР ШРИФТА В НАЗВАНИИ:', data.fontCase)
+    createBriefItem(content, '4. ДОПОЛНИТЕЛЬНЫЕ НАДПИСИ, КОТОРЫЕ ДОЛЖНЫ ПРИСУТСТВОВАТЬ В ЛОГОТИПЕ', data.slogan)
     createBriefItem(content, '6. ПРЕДПОЧИТАЕМЫЙ СТИЛЬ ЛОГОТИПА:', getTrueChecked(optionsLogo))
     
     pageWidth = document.documentElement.scrollWidth
     pageHeight = document.documentElement.scrollHeight
-    console.log('Pagesize:' + pageWidth + ' x ' + pageHeight)
+    console.log('Page size:' + pageWidth + ' x ' + pageHeight)
 
     // console.log(`Размер content: ${content.offsetHeight} X ${content.offsetWidth}`)
     
     const opt = {
         margin: 1,
-        filename: `${data.companyName}_logo_brief_Cliptic`,
+        filename: `${data.companyName}_logo_brief_Cliptic.jpg`,
         image: { type: 'jpeg', quality: 0.5 },
         html2canvas:  {
-            // width: pageWidth,
-            // height: pageHeight,
+            width: 800,
+            height: 1100,
             scale: 2,
             removeContainer: true
         },
@@ -84,11 +89,9 @@ function retrieveFormValue(event) {
     }
 
     // console.log(`opt: ${opt.jsPDF.format[0]} x ${opt.jsPDF.format[1]}`)
-    // вызов меточа для сохранения в pdf
-    html2pdf()
-        .set(opt)
-        .from(printBox)
-        .save()
+
+    //* вызов метода для сохранения в pdf
+    html2pdf().set(opt).from(printBox).save()
 }
 
 // проверка на checkbox и radio
@@ -100,7 +103,10 @@ function isCkeckboxOrRadio(type) {
 function createBriefItem(dadElement, text, formValue, classItem = 'brief-form__item', classContent = 'brief-form__label') {
     const briefItem = createElement('div', classItem)
     const itemContent = createElement('div', classContent)
-    itemContent.textContent = `${text} ${formValue}`
+    const itemFormValue = createElement('span', 'form-value')
+    itemFormValue.textContent = formValue
+    itemContent.textContent = ` ${text}`
+    itemContent.appendChild(itemFormValue)
     briefItem.appendChild(itemContent)
     dadElement.appendChild(briefItem)
 }
@@ -111,3 +117,32 @@ function createElement(type, className) {
     element.className = className
     return element
 }
+
+// навешивает событие click на блок targetClassName с radio/checkbox исключая наложенные сверху блоки exceptionClassName
+function onOffCheckedList(targetClassName, inputClassName, onClassName) {
+    const elems = document.querySelectorAll(`.${targetClassName}`)
+    for (item of elems) {
+        item.addEventListener('click', (event) => {
+            // onOffClass(elems, onClassName)
+            if (event.target.className === targetClassName) {
+                const elem = event.target
+                elem.querySelector(`.${inputClassName}`).checked = true
+                // console.log(elem)
+                // elem.classList.toggle(onClassName)
+            } else {
+                const elem = event.target.parentNode
+                elem.querySelector(`.${inputClassName}`).checked = true
+                // console.log(elem)
+                // elem.classList.toggle(onClassName)
+            }
+        })
+    }
+}
+
+// function onOffClass(elems, onClassName) {   
+//     for (elem of elems) {
+//         if (!elem.querySelector('input').checked) elem.classList.remove(onClassName)
+//     }
+//     console.log('onOffClass')
+// }
+
