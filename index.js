f7388525f8feadf8a80814d36320d086e86f44a5
Anton Karpov <81901285+dulkahakana@users.noreply.github.com>
2021-12-09 18:45:46 +0300

    на сервер. рабочая версия

--- index.js
+++ index.js
@@ -24,8 +24,6 @@ document.addEventListener("DOMContentLoaded", () => {
     // прослушивание собитий
     briefForm.addEventListener('submit', retrieveFormValue)
     onOffCard('logo-options__item', 'logo-options__input')
-    // onOffCheckedList('style-options__item', 'style-options__input')
-    // TODO только style-options__item' работает через рекурсию
     onOffCard('style-options__item', 'style-options__input')
     onOffCard('icon-options__item', 'icon-options__input')
     onOffCard('preferred-style-options__item', 'preferred-style-options__input')
@@ -85,16 +83,15 @@ document.addEventListener("DOMContentLoaded", () => {
         createBriefItem('4. КРИТИЧЕН ЛИ ДЛЯ ВАС ТАКОЙ ПАРАМЕТР КАК РЕГИСТР ШРИФТА В НАЗВАНИИ:', data.fontCase)
         createBriefItem('5. ДОПОЛНИТЕЛЬНЫЕ НАДПИСИ, КОТОРЫЕ ДОЛЖНЫ ПРИСУТСТВОВАТЬ В ЛОГОТИПЕ:', data.slogan)
         
-        // TODO в полях где есть возможность выбрать другое нужно сделать text input
         const groupStyleType = createElement('div', 'group-options__container')  
-        createBriefItem('6. ТИП ЛОГОТИПА:', getTrueChecked(logoOptions), 'form-value__options', groupStyleType)
-        createBriefItem('7. СТИЛЬ ЛОГОТИПА:', getTrueChecked(styleOptions), 'form-value__options', groupStyleType)        
-        createBriefItem('8. ТИП ЗНАКА:', getTrueChecked(iconOptions), 'form-value__options', groupStyleType)
+        createBriefItem('6. ТИП ЛОГОТИПА:', getOptionsGroup(logoOptions), 'form-value__options', groupStyleType)
+        createBriefItem('7. СТИЛЬ ЛОГОТИПА:', getOptionsGroup(styleOptions), 'form-value__options', groupStyleType)        
+        createBriefItem('8. ТИП ЗНАКА:', getOptionsGroup(iconOptions), 'form-value__options', groupStyleType)
         content.appendChild(groupStyleType)
 
         const groupIconFont = createElement('div', 'group-options__container')
-        createBriefItem('9. СТИЛЬ ОФОРМЛЕНИЯ ЗНАКА:', getTrueChecked(preferredStyleOptions), 'form-value__options', groupIconFont)
-        createBriefItem('10. СТИЛЬ ШРИФТОВОГО РЕШЕНИЯ:', getTrueChecked(fontOptions), 'form-value__options', groupIconFont)
+        createBriefItem('9. СТИЛЬ ОФОРМЛЕНИЯ ЗНАКА:', getOptionsGroup(preferredStyleOptions), 'form-value__options', groupIconFont)
+        createBriefItem('10. СТИЛЬ ШРИФТОВОГО РЕШЕНИЯ:', getOptionsGroup(fontOptions), 'form-value__options', groupIconFont)
         content.appendChild(groupIconFont)
 
         createBriefItem('11. ЦВЕТОВОЕ РЕШЕНИЕ ЛОГОТИПА:', data.colors)
@@ -102,16 +99,7 @@ document.addEventListener("DOMContentLoaded", () => {
         createBriefItem('13. КЛЮЧЕВЫЕ СЛОВА:', data.keywords)
         createBriefItem('14. ДОПОЛНИТЕЛЬНАЯ ИНФОРМАЦИЯ:', data.additionalInfo)
 
-        
-
-        
-        // pageWidth = document.documentElement.scrollWidth
-        // pageHeight = document.documentElement.scrollHeight
-        // console.log('Page size:' + pageWidth + ' x ' + pageHeight)
-
-        // console.log(`Размер content: ${content.offsetHeight} X ${content.offsetWidth}`)
-        
-        // при DPI = 150, разрешение формата А4 имеет 1240 × 1754 пикселей; при DPI = 300, разрешение формата А4 имеет 2480 × 3508 пикселей.
+                        
         // opt объект конфигурации html2pdf
         const opt = {
             margin: [1, 2, 1, 2],
@@ -137,20 +125,12 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         
-        // возвращает блок вывода в pdf в начало страницы
+        // возвращает страницу вывода в pdf в начало страницы
         // спасает от рассыпания pdf 
-        // window.scrollTo(0, 0)
+        // window.scrollTo(0, 0) // не работает на мобилках
         title.scrollIntoView(top)
         //* вызов метода для сохранения в pdf
         html2pdf().set(opt).from(content).save()
-
-        // TODO нельзя просто взять и не дождаться выполения печати в pdf (async)
-        // wrapperForm.classList.add('show')
-        // wrapperForm.classList.remove('hide')
-
-        // wrapperContent.classList.remove('show')
-        // wrapperContent.classList.add('hide')
-        // content.innerHTML = ''
     }
 
     // проверка на checkbox и radio
@@ -177,7 +157,6 @@ document.addEventListener("DOMContentLoaded", () => {
         if (typeof formValue === 'string') {
             itemFormValue.textContent = formValue
         } else {
-            // TODO Здесь формируются блоки с Img 
             briefItem.appendChild(formValue)            
         }        
         
@@ -185,16 +164,13 @@ document.addEventListener("DOMContentLoaded", () => {
         dadElement.appendChild(briefItem)
     }
 
-    // TODO нет описания функции
-    // TODO нужно выводить по два блока в одну строку
-    function getTrueChecked(checkedList) {
-        // const result = []
+    // возвращает dom элемент с выбранными опциями
+    function getOptionsGroup(checkedList) {
         const trueOptionsItem = createElement('div', 'container__to-pdf')
         for (option of checkedList) {
             if (option.checked) {
                 const optionsItem = createElement('div', 'item__to-pdf')
                 const elem = option.parentNode
-                // TODO возникает проблема если внутри блока нет тегов img label
                 const imgElem = elem.querySelector('img').cloneNode()
                 const labelElem = elem.querySelector('label').cloneNode(true)                
                 labelElem.classList = ''
@@ -212,8 +188,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const element = document.createElement(type)
         element.className = className
         return element
-    }
-        
+    }        
 
     // добавление события на options__input с помощью рекурсии getCheckbox
     function onOffCard(targetClassName, inputClassName) {
@@ -241,28 +216,6 @@ document.addEventListener("DOMContentLoaded", () => {
         let elemInput = elem.querySelector(`.${inputClassName}`)
         elemInput.checked ? elemInput.checked = false : elemInput.checked = true
     }
-    
-    // навешивает событие click на блок targetClassName 
-    // для переключения внутри radio/checkbox с классами inputClassName
-    // function onOffCheckedList(targetClassName, inputClassName) {
-    //     const elems = document.querySelectorAll(`.${targetClassName}`)
-    //     for (item of elems) {
-    //         item.addEventListener('click', (event) => {
-    //             if (event.target.className.includes(targetClassName)) {
-    //                 const elem = event.target
-    //                 onOffChecked(elem, inputClassName)                    
-    //             } else if (!event.target.className.includes(targetClassName) && !event.target.className.includes(inputClassName) && event.target.tagName !== 'IMG') {
-    //                 const elem = event.target.parentNode
-    //                 onOffChecked(elem, inputClassName)
-    //             } else if (event.target.tagName === 'IMG') {
-    //                 const divImg = event.target.parentNode
-    //                 const elem = divImg.parentNode
-    //                 onOffChecked(elem, inputClassName)
-    //             }
-    //         })
-    //     }
-    // }
-
 
 }); /* DOMContentLoaded */
 
